5b8d092f4df04170685e8be66da68efe5931e279
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-09-14 12:53:37 +0200

    custom preview button

--- src/plugins/preview-button/admin/src/components/PreviewLink/index.js
+++ src/plugins/preview-button/admin/src/components/PreviewLink/index.js
@@ -9,13 +9,21 @@
       if (!initialData.slug) {
         return null;
       }
+
+      var type = window.location.pathname.substring(
+        window.location.pathname.indexOf(".") + 1, 
+        window.location.pathname.lastIndexOf("/")
+      );
+
+      console.log(type)
     
       return (
         <LinkButton
           size="S"
           startIcon={<Eye/>}
           style={{width: '100%'}}
-          href={`${CLIENT_FRONTEND_URL}?secret=${CLIENT_PREVIEW_SECRET}&slug=${initialData.slug}`}
+          // href={`${CLIENT_FRONTEND_URL}?secret=${CLIENT_PREVIEW_SECRET}&slug=${initialData.slug}`}
+          href={`${CLIENT_FRONTEND_URL}/${type.replace('discover', 'archive')}/${initialData.slug}?preview=true`}
           variant="secondary"
           target="_blank"
           rel="noopener noreferrer"
